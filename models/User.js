78809eb31275f4e06d64d2cd8136983ffe06b29c
Graiemh <qg.guinier@gmail.com>
2023-10-06 10:36:03 +0200

    Repository creation, db connect and user model generation

---
+++ models/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const UsersSchema = mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+
+  },
+  { versionKey: false }
+);
+
+const collectionName = 'users'
+const UsersModel = mongoose.model('users', UsersSchema, collectionName)
+
+
+export default UsersModel
